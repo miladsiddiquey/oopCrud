6f99d9b4560f90438aefccd93ebf92f4524ea065
Milad Siddiquey <miladsiddiquey@gmail.com>
2024-07-01 02:15:16 +0600

    update code

--- database.php
+++ database.php
@@ -1,4 +1,3 @@
-
 <?php
 
 class Database{
@@ -46,18 +45,157 @@ public function insert($table, $params=array()){
 }
 
 //Function to update row in database
-public function update(){
+public function update($table, $params=array(),$where = null){
+    if($this->tableExists($table)){
+        $args = array();
+        foreach($params as $key => $value){
+            $args[] = "$key = '$value'";
+        }
+        $sql = "UPDATE $table SET" . implode(', ', $args);
+        if($where != null){
+            $sql .= " WHERE $where";
+        }
 
+        if($this->mysqli->query($sql)){
+            array_push($this->result, $this->mysqli->affected_rows);
+			return true;
+        }else{
+            array_push($this->result, $this->mysqli->error);
+			return false;
+        }
+
+    }else{
+        return false;
+    }
 }
 
 //Function to delete table or row from database
-public function delete(){
+public function delete($table, $where = null){
+    if($this->tableExists($table)){
+        $sql = "DELETE FROM $table";
+        if($where != null){
+            $sql .= " WHERE $where";
+        }
+        if($this->mysqli->query($sql)){
+            array_push($this->result, $this->mysqli->affected_rows);
+			return true;
+        }else{
+            array_push($this->result, $this->mysqli->error);
+			return false;
+        }
 
+    }else{
+        return false;
+    }
 }
 
 //Function to select from the database
-public function select(){
+public function select($table, $rows = "*", $join = null, $where = null, $order=null,$limit=null){
+    if($this->tableExists($table)){
+        $sql = "SELECT $rows FROM $table";
+        if($join != null){
+            $sql .= " JOIN $join";
+        }
+        if($where != null){
+            $sql .= " WHERE $where";
+        }
+        if($order != null){
+            $sql .= " ORDER BY $order";
+        }
+        if($limit != null){
+			if(isset($_GET['page'])){
+				$page = $_GET['page'];
+			}else{
+				$page = 1;
+			}
+			$start = ($page - 1) * $limit;
+            $sql .= " LIMIT $start, $limit";
+        }
 
+        $query = $this->mysqli->query($sql);
+
+    if($query){
+        $this->result = $query->fetch_all(MYSQLI_ASSOC);
+        return true;
+    }else{
+        array_push($this->result, $this->mysqli->error);
+        return false;
+    }
+    
+    }else{
+        return false;
+    }
+}
+
+public function pagination($table, $join = null, $where = null, $limit=null){
+	if($this->tableExists($table)){
+		if($limit != null){
+			$sql = " SELECT COUNT(*) FROM $table";
+			if($join != null){
+				$sql .= " JOIN $join";
+			}
+			if($where != null){
+				$sql .= " WHERE $where";
+			}
+
+			$query = $this->mysqli->query($sql);
+
+			$total_record = $query->fetch_array();
+			$total_record = $total_record[0];
+
+			$total_page = ceil($total_record / $limit);
+
+			$url = basename($_SERVER['PHP_SELF']);
+
+			if(isset($_GET['page'])){
+				$page = $_GET['page'];
+			}else{
+				$page = 1;
+			}
+
+			$output = "<ul class='pagination'>";
+
+			if($page>1){
+				$output .= "<li><a  href='$url?page=".($page - 1)."'>Prev</a></li>";
+			}
+			if($total_record > $limit){
+				for($i = 1; $i <= $total_page; $i++){
+					if($i == $page){
+						$cls = "class='active'";
+					}else{
+						$cls = "";
+					}
+					$output .= "<li><a $cls href='$url?page=$i'>$i</a></li>";
+				}
+			}
+			if($total_page>$page){
+				$output .= "<li><a  href='$url?page=".($page + 1)."'>Next</a></li>";
+			}
+
+			$output .= "</ul>";
+
+			return $output;
+			
+
+		}else{
+			return false;
+		}
+	}else{
+		return false;
+	}
+}
+
+
+public function sql($sql){
+    $query = $this->mysqli->query($sql);
+
+    if($query){
+        $this->result = $query->fetch_all(MYSQLI_ASSOC);
+        return true;
+    }else{
+        array_push($this->result, $this->mysqli->error);
+        return false;
+    }
 }
 
 private function tableExists($table){
--- index.php
+++ index.php
@@ -2,8 +2,28 @@
 include "database.php";
 $obj = new Database();
 
-$obj->insert('client', ['name' =>'milad siddiquey', 'address'=>'bangladesh']);
-echo "insert result is: ";
-print_r($obj->getResult());
+// insert data
+// $obj->insert('client', ['name' =>'Sayem Hossain', 'address'=>'Sylhet']);
+// echo "insert result is: ";
+// print_r($obj->getResult());
+
+//update
+// $obj->update('client', ['name' =>'milad siddiquey', 'address'=>'bangladesh'],'id="5"');
+// echo "update result is: ";
+// print_r($obj->getResult());
+
+//delate
+// $obj->delate('client','id="6"');
+// echo "update result is: ";
+// print_r($obj->getResult());
+
+//select
+
+// $obj->select('client','*',null,null,null,2);
+// echo "select result is: ";
+// print_r($obj->getResult());
+
+// echo $obj->pagination('client',null,null,2);
+
 
 ?>
